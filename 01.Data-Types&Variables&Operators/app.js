424685b5374d8f870d18f9fca61d1de65a800579
aghalarova <agalarovasbin76@gmail.com>
2023-10-30 08:54:18 -0700

    app

---
+++ 01.Data-Types&Variables&Operators/app.js
@@ -0,0 +1,60 @@
+// Task 1
+let firstName = "Sabina";
+let lastName = "Aghalarova";
+let country = "Barda";
+let age = "20";
+let job = "student";
+let fullInformation =
+  firstName +
+  " " +
+  lastName +
+  ". " +
+  "I live in" +
+  " " +
+  country +
+  ". " +
+  "I am" +
+  " " +
+  age +
+  " " +
+  "years old" +
+  " . " +
+  " I am a" +
+  " " +
+  job;
+console.log(fullInformation);
+
+// Task 2
+let length = 16;
+console.log(typeof length);
+
+let color = "brown";
+console.log(typeof color);
+
+let float = "12.5";
+console.log(typeof float);
+
+let like = true;
+console.log(typeof like);
+
+// Task 3
+let distance = 8600;
+let speed = 100;
+let duration = distance / speed;
+console.log(
+  "The car will arrive in Chine in" + " " + duration + " " + "minutes"
+);
+
+// Task 4
+let birthYear = 2003;
+let thisYear = 2023;
+let Age = thisYear - birthYear;
+console.log("I will be either" + " " + Age + " " + "or" + " " + (Age + 1));
+
+// Task 5
+let Sabina = 90;
+let Aziza = 90;
+let Qanira = 95;
+let Nargiz = 90;
+let ortalama = ((Sabina + Aziza + Qanira + Nargiz) / 4);
+console.log(ortalama);
