593cd1724ec82cdd2b015eca7208e3c94bc82a90
aghalarova <agalarovasbin76@gmail.com>
2023-10-31 08:50:33 -0700

    algoritm

---
+++ 31.10.CONTROLSTRUCTUR-ALGORITM/main.js
@@ -0,0 +1,79 @@
+// TASK-1
+// let weather = 10;
+
+// if (weather >= 22) {
+//   console.log(`Nazik geyin`);
+// } else if (weather >= 15) {
+//   console.log(`pencek geyin`);
+// } else {
+//   console.log(`qalin geyin`);
+// }
+
+// TASK 2
+
+// let letter = "g" ;
+//  if (letter === "a" || letter === "i" || letter === "e" ){
+//     console.log("saitdir");
+//  }else if (letter === "b" || letter === "c" || letter === "d" || letter === "f" ){
+//     console.log("samitdir");
+//  }else {
+//     console.log("herf deyil");
+//  }
+
+//  TASK 3
+
+// let a = 105;
+// let b = 52;
+// let c = 88;
+
+// if(a>b && a>c){
+//     console.log(`${a} is greater`);
+// }else if (b>a && b>c){
+//     console.log(`${b} is greater`);
+// }else {
+//     console.log(`${c} is greater`);
+// }
+
+// TASK 4
+
+// let score = 55;
+// if (score > 90) {
+//   console.log(`A`);
+// } else if (score > 80) {
+//   console.log(`B`);
+// } else if (score > 70) {
+//   console.log(`C`);
+// } else if (score > 60) {
+//   console.log(`D`);
+// } else if (score > 50) {
+//   console.log(`F`);
+// } else {
+//   console.log(`kesr`);
+// }
+
+// TASK 5
+
+// let firstName = "Sabina";
+// if (firstName.length <= 5) {
+//   console.log(`Salam ${firstName}. Qisa adiniz var`);
+// } else if (firstName.length <= 10) {
+//   console.log(`Salam ${firstName}. Orta uzunluqlu adiniz var`);
+// } else {
+//   console.log(`Salam ${firstName}. Adiniz cox uzundur `);
+// }
+
+// TASK 6
+
+let a = 10;
+let b = 7;
+let c = 5;
+if (a + b > a + c && a + b > b + c) {
+  console.log(`${a} + ${b} = ${a + b} is biggest`);
+} else if (a + c > a + b && a + c > b + c) {
+  console.log(`${a} + ${c} = ${a + c} is biggest`);
+} else {
+  console.log(`${b} + ${c} = ${b + c} is biggest`);
+}
+
+
+
