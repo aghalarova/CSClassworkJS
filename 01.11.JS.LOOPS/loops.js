053ad6180cf6ecc9fb5b08029f5d893e5575030d
aghalarova <agalarovasbin76@gmail.com>
2023-11-01 08:46:15 -0700

    construls

---
+++ 01.11.JS.LOOPS/loops.js
@@ -0,0 +1,59 @@
+// TASK-1
+// for (i = 1; i < 100; i++) {
+//   if (i % 2 === 0) console.log(i);
+// }
+
+// TASK-2
+// let str = "Sabina";
+// for (i = 0; i < str.length; i++) {
+//   console.log(str[i]);
+// }
+
+// TASK-3
+// for(i=10;i<100;i++){
+//     if(i%10===7)
+//     console.log(i);
+// }
+
+// TASK-4
+// for(i=11;i<100;i++){
+//     if(i%11===0)
+//     console.log(i);
+// }
+
+// TASK-5
+// let number = 30;
+// for (i = 1; i <= 30; i++) {
+//   if (number % i == 0) console.log(i);
+// }
+
+// TASK-6
+// let m = 6;
+// let factorial = 1;
+// for (i = 1; i <= m; i++) {
+//   factorial *= i;
+// }
+// console.log(factorial);
+
+// TASK-7
+// let sum = 0;
+// for (let i = 1; i < 10; i++) {
+//   if (i % 2 == 0) {
+//     sum += i;
+//   }
+// }
+// console.log(sum);
+
+// TASK-8
+// let sum = 0;
+// for (i = 10; i < 50; i++) {
+//   if (i % 3 == 0) {
+//     sum += i;
+//   }
+// }
+// console.log(sum);
+
+// HOME-TASK-1
+for (i = 0; i <= 10; i++) {
+  console.log(`${i}*${i} = ${i * i}`);
+}
