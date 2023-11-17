73aaa8f6c6ecbe4cf709e2bcf4900216d2e4a056
aghalarova <agalarovasbin76@gmail.com>
2023-11-17 07:47:33 -0800

    java

---
+++ 17.11JavaScript.practices/index.js
@@ -0,0 +1,15 @@
+// TASK-1
+function isPalindrome(num) {
+  let result = String(num).split("").reverse().join("");
+  return result;
+}
+
+// console.log(isPalindrome(46884));
+
+// TASK-2
+let word = "webmaster";
+let result = word.split("").sort().join();
+// console.log(result);
+// console.log(word.split("").sort().join());
+
+// TASK-3
