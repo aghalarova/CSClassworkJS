0f0517b922c02d1014a38bc82fc619398666ece3
aghalarova <146707658+aghalarova@users.noreply.github.com>
2024-01-10 12:52:43 +0400

    furnish

--- 09.01.Furnish/assets/js/index.js
+++ 09.01.Furnish/assets/js/index.js
@@ -1,23 +1,41 @@
 const BASE_URL = "http://localhost:8080";
-const cards = document.querySelector(".banner-cards");
+const cards = document.querySelector(".cards-sect");
+const searchInput = document.querySelector("input");
+let arr;
+
 async function getData() {
-  fetch(`${BASE_URL}/products`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-        drawProducts(data);
-    });
+  const response = await axios(`${BASE_URL}/products`);
+  console.log(response.data);
+  arr = response.data;
+
+  drawProducts(response.data);
 }
 getData();
+
 function drawProducts(data) {
   cards.innerHTML = "";
   data.forEach((element) => {
     cards.innerHTML += `
-    <div class="banner-card">
-        <img src="${element.image}" alt="">
+    <div class="card">
+        <i class="fa-regular fa-handshake"></i>
         <h2>${element.title}</h2>
         <p>${element.description}</p>
+        <div class="icon">
+          <button><i class="fa-regular fa-heart"></i></button>
+          <button><i class="fa-regular fa-pen-to-square"></i></button>
+          <button class="btn-card">
+            <i class="fa-regular fa-trash-can"></i>
+          </button>
+        </div>
     </div>
       `;
   });
 }
+
+searchInput.addEventListener("input", function (e) {
+  e.preventDefault();
+  let filtered = arr.filter((item) =>
+    item.title.toLowerCase().includes(e.target.value.toLowerCase())
+  );
+  drawProducts(filtered);
+});
