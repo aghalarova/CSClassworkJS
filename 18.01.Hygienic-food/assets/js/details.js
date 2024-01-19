18b1bd4a1c1cf965c80392e933166d94b744193e
aghalarova <146707658+aghalarova@users.noreply.github.com>
2024-01-19 14:56:39 +0400

    app

---
+++ 18.01.Hygienic-food/assets/js/details.js
@@ -0,0 +1,2 @@
+const BASE_URL = "http://localhost:8080";
+const dataCard= document.querySelector(".")
--- 18.01.Hygienic-food/assets/js/main.js
+++ 18.01.Hygienic-food/assets/js/main.js
@@ -1,10 +1,18 @@
 const BASE_URL = "http://localhost:8080";
 const cardsData = document.querySelector(".cards-data");
+const btnOne = document.querySelector(".btn-1");
+const btnTwo = document.querySelector(".btn-2");
+const btnThird = document.querySelector(".btn-3");
+const btnFour = document.querySelector(".btn-4");
+const searchInput = document.querySelector("#search-input");
 
 async function getData() {
   const response = await axios(`${BASE_URL}/products`);
-  console.log(response.data);
-  drawCards(response.data);
+  arr = response.data;
+  let filtered = arr.filter(
+    (item) => item.catagory.toLocaleLowerCase() === "breakfast"
+  );
+  drawCards(filtered);
 }
 getData();
 
@@ -19,8 +27,52 @@ function drawCards(data) {
               </div>
               <div class="text-card-right">
                 <span>${element.price}</span>
+                <a href="../../details.html?id=${element.id}">View details</a>
               </div>
             </div>
      `;
   });
 }
+
+btnOne.addEventListener("click", function () {
+  let filtered = arr.filter(
+    (item) => item.catagory.toLocaleLowerCase() === "breakfast"
+  );
+  drawCards(filtered);
+});
+
+btnTwo.addEventListener("click", function () {
+  let filtered = arr.filter(
+    (item) => item.catagory.toLocaleLowerCase() === "brunch"
+  );
+  drawCards(filtered);
+});
+
+btnThird.addEventListener("click", function () {
+  let filtered = arr.filter(
+    (item) => item.catagory.toLocaleLowerCase() === "lunch"
+  );
+  drawCards(filtered);
+});
+
+btnFour.addEventListener("click", function () {
+  let filtered = arr.filter(
+    (item) => item.catagory.toLocaleLowerCase() === "dinner"
+  );
+  drawCards(filtered);
+});
+
+// async function deleteCard(id, btn) {
+//   if (confirm("?")) {
+//     await axios.delete(`${BASE_URL}/products/${id}`);
+//     btn.parentElement.remove();
+//   }
+// }
+
+// searchInput.addEventListener("input", function (e) {
+//   e.preventDefault();
+//   let searchFiltered = arr.filter((item) =>
+//     item.title.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase())
+//   );
+//   drawCards(searchFiltered);
+// });
